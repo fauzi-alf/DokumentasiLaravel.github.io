7b5affe2df00ed48d14f073ca5b9f778bcc71f82
ARZKUN <aruharishi@gmail.com>
2024-02-21 09:28:45 +0700

    upload pertama

---
+++ script.js
@@ -0,0 +1,13 @@
+
+      
+      // Efek paralaks pada gambar anime di bagian hero
+      var heroImage = document.querySelector('.hero img');
+      window.addEventListener('scroll', function() {
+        var scrollPosition = window.pageYOffset;
+        heroImage.style.transform = `translateY(${scrollPosition * 0.3}px)`;
+      });
+
+
+    
+
+    
